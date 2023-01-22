98a56a35a2e852457b3c5bc37d90cbd684343ad1
OUERTANI Mohamed Hachem <omhx21@gmail.com>
2023-01-22 13:04:25 +0100

    Intermediary changes

---
+++ src/graphutils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+// pragma once should work with almost all modern compilers.
+#include 
+namespace Graphutils {
+    struct graph {
+	
+    }
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// Main
+int main(int argc, char *argv[])
+{
+	std::cout << "PI_Project" << std::endl;
+	return EXIT_SUCESSS;
+}
