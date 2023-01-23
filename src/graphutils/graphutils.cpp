d03a705afaf22e2050f621559461e23a5989c866
OUERTANI Mohamed Hachem <omhx21@gmail.com>
2023-01-23 22:23:43 +0100

    Not ready yet, mod2

--- src/graphutils/graphutils.cpp
+++ src/graphutils/graphutils.cpp
@@ -1,28 +1,30 @@
 #include "graphutils.hpp"
 
-std::ostream &operator<<(std::ostream &os, std::list<int> adjs) {
-  if (adjs.size() == 0) {
+// For now this print implementation does not show the label. This can be easily modified later.
+std::ostream &operator<<(std::ostream &os, std::list<Graphutils::cell> neighs) {
+  if (neighs.size() == 0) {
     os << "[]";
-  } else if (adjs.size() == 1) {
-    os << "[" << adjs.front() << "]";
-  } else {
-    auto before_last = --adjs.end();
+  } /*else if (neighs.size() == 1) {
+    os << "[" << neighs.front() << "]";
+  } */else {
+    auto before_last = --neighs.end();
     os << "[";
-    for (auto it = adjs.begin(); it != before_last; it++) {
-      int neighbour = *it;
-      os << neighbour << ",";
+    for (auto it = neighs.begin(); it != before_last; it++) {
+      Graphutils::cell neighbour = *it;
+      os << "(" << neighbour.vertex << "," << neighbour.weight << ")" << ";";
     }
-    os << adjs.back() << "]";
+    os << "(" << neighs.back().vertex << "," << neighs.back().weight << ")" << "]";
   }
+
   return os;
 }
 
 std::ostream &operator<<(std::ostream &os, Graphutils::graph const &m) {
 
-  for (int vert = 0; vert < m.adjs.size() - 1; vert++) {
-    std::list<int> adj = m.adjs[vert];
+  for (int vert = 0; vert < m.info.size() - 1; vert++) {
+    std::list<Graphutils::cell> adj = m.info[vert].second;
     os << vert << ":" << adj << std::endl;
   }
-  os << m.adjs.size() - 1 << ":" << m.adjs[m.adjs.size() - 1];
+  os << m.info.size() - 1 << ":" << m.info[m.info.size() - 1].second;
   return os;
 }
--- src/graphutils/graphutils.hpp
+++ src/graphutils/graphutils.hpp
@@ -1,15 +1,79 @@
-#pragma once
-// pragma once should work with almost all modern compilers.
+#pragma once // pragma once should work with almost all modern compilers.
 #include <iostream>
 #include <vector>
 #include <list>
 
+
+
 namespace Graphutils {
-	struct graph {
-		std::vector<std::list<int>> adjs;
+
+	/// @brief This is an intermediary definition used to make working with graph adjacency lists easier.
+	struct cell  {
+		int vertex;
+		double weight;
+		void* _p; // Points to the cell from vertex's adjacency lists that points to this cell as well.
+	};
+	
+	/// @brief This is a label attached to a vertex. It could be the name of a city, a number, ...
+	struct label {
+		int index; // For the moment we just keep a number, and identify it to the index of creation of the graph, which should be unique
 	};
 
+	/// This is an augmented adjacency list representattion - every element {y,w,p} of an adjacency list to a vertex x contains : 
+	/// The index of the adjacent point, the weight connecting them, and a pointer to the triplet {x,w,p'} from y's adjacency list.
+	/// The lists being double-linked, this would mean our deleting a link would be -slightly- faster.
+	struct graph {
+		int _number_of_created_vertices = 0; 
+		int number_of_vertices = 0;
+		int number_of_edges = 0;
+		std::vector<std::pair<label, std::list<cell>>> info; // For each vertex, we associate a list of (connected_vertex, weight).
+		// I use void* because a recursive definition of the types would be much more messy.
+		// We therefore try to be careful when verifying the integrity of the code.
+		// For _number_of_created_vertices: this number goes up and never down, and does up when a new vertex is created.
+		// It is currently used to modify the label to keep it unique for each vertex.
+	};
+
+	/// @brief Creates an empty graph
+	/// @return Empty Graph.
 	graph empty_graph();
+
+	/// @brief Adds a vertex. Returns the number corresponding to the vertex
+	/// @param g 
+	int add_vertex(graph& g);
+
+	/// @brief Removes an index and all the weights connecting to it. 
+	/// @param g Graph
+	/// @param vertex Vertex number (from 0 to number_of_vertices-1)
+	void remove_vertex(graph& g, int vertex);
+	
+	/// @brief Disconnects a vertex from the graph. It will therefore become connected to NO other vertex.
+	/// @param g Graph
+	/// @param vertex Vertex number (from 0 to number_of_vertices-1)
+	void disconnect_vertex(graph& g, int vertex);
+
+	/// @brief Adds an edge to the Graph g linking vertex1 and vertex2 with the cost weight 
+	/// @param g Graph
+	/// @param vertex1 First vertex
+	/// @param vertex2 Second vertex
+	/// @param weight Weight between both vertices.
+	void add_edge(graph& g, int vertex1, int vertex2, double weight);
+
+	/// @brief Removes the edge (vertex1,vertex2) from the graph
+	/// @param g Graph
+	/// @param vertex1 First vertex
+	/// @param vertex2 Second vertex
+	void remove_edge(graph& g, int vertex1, int vertex2);
+
+	/// @brief Adds the vertices that are missing. 
+	/// @param g 
+	/// @param vertex1 
+	/// @param vertex2 
+	/// @param weight 
+	void add_link(graph& g, int vertex1, int vertex2, double weight);
+	/// @brief Allows to print the graph in a readable format (for std::cout for example)
+	/// @param os Output stream
+	/// @param m The graph to print
+	/// @return Output stream.
 	std::ostream &operator<<(std::ostream &os, graph const &m);
 }
 
